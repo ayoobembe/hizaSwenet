7723856590a29395e30ffcaf453eefabe44746cf
Ayo <ayo.obembe@gmail.com>
2015-02-10 12:16:33 +0000

    included express, serving up static hello world index page

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/app'))
+
+var server = app.listen(8000, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Server listening on port %s', port)
+})
